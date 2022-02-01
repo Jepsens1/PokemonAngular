0c0fa214d0d6eb084b8543b8ef890211151dc172
Phillip Svanholt Kjær Jepsen <jepsens123@outlook.dk>
2022-02-01 13:38:58 +0100

    changed so the api needs a apikey so access

--- src/app/pokemon-search/pokemon-search.component.ts
+++ src/app/pokemon-search/pokemon-search.component.ts
@@ -12,27 +12,27 @@ export class PokemonSearchComponent implements OnInit {
 
   pokemons$!: Observable<Pokemon[]>;
   private searchTerms = new Subject<string>();
-  constructor(private pokemonService: PokemonService) {}
-  
+  constructor(private pokemonService: PokemonService) { }
+
   search(term: string): void {
     this.searchTerms.next(term);
   }
-  SubmitResult(): void{
+  SubmitResult(): void {
     this.pokemonService.searchPokemons("bulbasaur").subscribe(x => {
 
     })
   };
   ngOnInit(): void {
-  //  this.pokemons$ = this.searchTerms.pipe(
+    this.pokemons$ = this.searchTerms.pipe(
       // wait 300ms after each keystroke before considering the term
-    //  debounceTime(300),
+      debounceTime(300),
 
       // ignore new term if same as previous term
-      //distinctUntilChanged(),
+      distinctUntilChanged(),
 
       // switch to new search observable each time the term changes
-      //switchMap((term: string) => this.pokemonService.searchPokemons(term)),
-    //);
+      switchMap((term: string) => this.pokemonService.searchPokemons(term)),
+    );
   }
 
 }
